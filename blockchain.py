631fb19979ab3eaf1ae1237c8505be7a7557f39b
Yaz Khoury <yaz.khoury@gmail.com>
2017-11-23 17:45:54 -0500

    feat: Add block generation function

--- blockchain.py
+++ blockchain.py
@@ -1,3 +1,7 @@
+import hashlib
+import json
+from time import time
+
 class Blockchain(object):
     """Blockchain Class to Store the Blockchain"""
 
@@ -5,9 +9,27 @@ class Blockchain(object):
         self.chain = []
         self.current_transactions = []
 
-    def new_block(self):
-        """Creates new block and adds it to the chain"""
-        pass
+        self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self, proof, previous_hash=None):
+        """
+        Creates a new block in the blockchain.
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash of previous block
+        :return: <dict> New Block
+        """
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1])
+        }
+
+        # Reset current list of transactions
+        self.current_transactions = []
+        self.chain.append(block)
+        return block
 
     def new_transaction(self, sender, recipient, amount):
         """
