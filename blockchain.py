b80a5e6875b60e8451a2a5905ebcc96fc3b1169f
Yaz Khoury <yaz.khoury@gmail.com>
2017-11-23 18:24:05 -0500

    feat: add flask routing for server

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,10 @@
 import hashlib
 import json
 from time import time
+from textwrap import dedent
+from uuid import uuid4
+
+from flask import Flask, jsonify, request
 
 class Blockchain(object):
     """Blockchain Class to Store the Blockchain"""
@@ -89,3 +93,44 @@ class Blockchain(object):
     def last_block(self):
         """Returns the last block in the chain"""
         return self.chain[-1]
+
+
+# Instantiate Our Node
+app = Flask(__name__)
+
+# Generate a globally unique address for this node
+node_identifier = str(uuid4().replace('-', ''))
+
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    return "We'll mine a new block"
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+
+    # Check that the required fields are in the POSTed Data
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return "Missing values", 400
+
+    # Create new transaction
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+    respone = {
+        'message': 'Transaction will be added to Block {}'.format(index)
+    }
+    return jsonify(response), 201
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain)
+    }
+
+    return jsonify(response), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
