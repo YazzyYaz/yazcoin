af87b61f96507933783bacc7c3c6472c1d3323c4
Yaz Khoury <yaz.khoury@gmail.com>
2017-11-23 17:36:25 -0500

    feat: Add code for returning index of new transaction gen

--- blockchain.py
+++ blockchain.py
@@ -9,9 +9,22 @@ class Blockchain(object):
         """Creates new block and adds it to the chain"""
         pass
 
-    def new_transaction(self):
-        """Adds a new transaction to the list of transactions"""
-        pass
+    def new_transaction(self, sender, recipient, amount):
+        """
+        Creates a new transaction to go into the next mined block.
+        :param sender: <str> Address of sender
+        :param recipient: <str> Address of the recipient
+        :param amount: <str> Amount
+        :return: <int>The index of the block that will hold this transaction
+        """
+
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        })
+
+        return self.last_block['index'] + 1
 
     @staticmethod
     def hash(block):
